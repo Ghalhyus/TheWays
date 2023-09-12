4d032927cd94740a172a9c69cc235d5e29464b15
SIMONIN David <ghalhyus@gmail.com>
2023-09-12 21:00:55 +0200

    First commit

---
+++ src/main/java/com/ghalhyus/web/theways/TheWaysApplication.java
@@ -0,0 +1,13 @@
+package com.ghalhyus.web.theways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheWaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheWaysApplication.class, args);
+    }
+
+}
